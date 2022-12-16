7151d05bc9881659da490e9c016b03968436cc34
mark <markichnich@proton.me>
2022-12-16 20:24:39 +0100

    shuffle around neovim config

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -1,41 +1,41 @@
-local opts = {noremap = true, silent = true}
-local t_opts = {noremap = true, silent = true}
-
-local map = vim.api.nvim_set_keymap
+local map = vim.keymap.set
 
 -- leader maps
-map("", "<space", "<nop>", opts)
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
+map('', '<space', '<nop>')
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
 
 -- normal mode
-map("n", "//", ":nohl<cr>", opts)
+map('n', '//', ':noh<cr>')
 
-map("n", "<leader>e", ":e %:h/<c-d>", opts)
-map("n", "<leader>f", ":History<cr>", opts)
-map("n", "<leader>y", ":%y+<cr>", opts)
-map("n", "<leader>d", "\"_d", opts)
+map('n', '<leader>E', vim.cmd.Ex)
+map('n', '<leader>y', ':%y+<cr>')
+map('n', '<leader>d', '"_d')
 
-map("n", "<c-d>", "10<c-d>zz", opts)
-map("n", "<c-u>", "10<c-u>zz", opts)
+map('n', '<c-d>', '10<c-d>zz')
+map('n', '<c-u>', '10<c-u>zz')
 
-map("n", "H", ":bprevious<cr>", opts)
-map("n", "L", ":bnext<cr>", opts)
+map('n', 'n', 'nzzzv')
+map('n', 'N', 'Nzzzv')
+
+map('n', 'H', ':bp<cr>')
+map('n', 'L', ':bn<cr>')
+
+map('n', '<', 'v<')
+map('n', '>', 'v>')
+
+map('n', '<leader>q', ':w<cr>:bd!<cr>')
 
 -- visual mode
-map("v", "<", "<gv", opts)
-map("v", ">", ">gv", opts)
+map('v', '<', '<gv')
+map('v', '>', '>gv')
 
-map("v", "<a-j>", ":m .+1<cr>==", opts)
-map("v", "<a-k>", ":m .-2<cr>==", opts)
-map("v", "p", '"_dP', opts)
+map('v', '<a-j>', ':m .+1<cr>==')
+map('v', '<a-k>', ':m .-2<cr>==')
+map('v', 'p',     '"_dP')
 
 -- visual block
-map("x", "J", ":move '>+1<cr>gv-gv", opts)
-map("x", "K", ":move '<-2<cr>gv-gv", opts)
-map("x", "<a-j>", ":move '>+1<cr>gv-gv", opts)
-map("x", "<a-k>", ":move '<-2<cr>gv-gv", opts)
-
--- terminal mode
-map("t", "<esc>", "<c-\\><c-n>", t_opts)
-
+map('x', 'J',     ":move '>+1<cr>gv-gv")
+map('x', 'K',     ":move '<-2<cr>gv-gv")
+map('x', '<a-j>', ":move '>+1<cr>gv-gv")
+map('x', '<a-k>', ":move '<-2<cr>gv-gv")
--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,37 +1,39 @@
 vim.cmd [[packadd packer.nvim]]
 
-require("packer")
+require('packer')
 .startup(function()
-    -- functionality
-    use("wbthomason/packer.nvim")
-    use("norcalli/nvim-colorizer.lua")
-
-    -- aesthetic
-    use("vv9k/vim-github-dark")
-    use("danishprakash/vim-yami")
-
-    vim.cmd([[colorscheme yami]])
-    -- vim.cmd [[highlight Normal ctermbg=NONE]]
-    vim.cmd [[highlight NonText ctermfg=14 guifg=#666666]]
-    vim.cmd [[highlight Whitespace ctermfg=14 guifg=#666666]]
-    -- vim.cmd [[highlight LineNr ctermbg=NONE]]
-    -- vim.cmd [[highlight clear SignColumn]]
-    -- vim.cmd [[highlight clear Tabline]]
-    -- vim.cmd [[highlight clear TablineFill]]
-    -- vim.cmd [[highlight clear TablineSel]]
-
-    -- treesitter
-    use("nvim-treesitter/nvim-treesitter")
-    require("nvim-treesitter.configs")
-    .setup({
-        ensure_installed = { "c", "lua", "rust" },
-        highlight = { enable = true },
+    use('wbthomason/packer.nvim')
+    use('norcalli/nvim-colorizer.lua')
+    use('vv9k/vim-github-dark')
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.0',
+        requires = { {'nvim-lua/plenary.nvim'} }
+    }
+    use({
+        'rose-pine/neovim',
+        as = 'rose-pine',
+        config = function()
+            vim.cmd('colorscheme rose-pine')
+        end
     })
-
-    -- fzf
-    use("junegunn/fzf")
-    use("junegunn/fzf.vim")
-
-    -- lsp
-    use("neovim/nvim-lspconfig")
+    use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+    use('junegunn/fzf')
+    use('junegunn/fzf.vim')
+    use('tpope/vim-fugitive')
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        requires = {
+            {'neovim/nvim-lspconfig'},
+            {'williamboman/mason.nvim'},
+            {'williamboman/mason-lspconfig.nvim'},
+            {'hrsh7th/nvim-cmp'},
+            {'hrsh7th/cmp-buffer'},
+            {'hrsh7th/cmp-path'},
+            {'saadparwaiz1/cmp_luasnip'},
+            {'hrsh7th/cmp-nvim-lsp'},
+            {'hrsh7th/cmp-nvim-lua'},
+            {'L3MON4D3/LuaSnip'},
+            {'rafamadriz/friendly-snippets'},
+        }
+    }
 end)
